4b3f356beccd2246fabfa0514e98cc8873605a1a
Estavio <louie.estella2135@gmail.com>
2024-07-14 20:15:48 +0800

    Yes

--- testmap/Scenes/Camera2D.gd
+++ testmap/Scenes/Camera2D.gd
@@ -1,8 +1,9 @@
 extends Camera2D
 
 var firstto2 = true
+var secondto3 = true
 
-func _on_scene_12_connector_body_exited(body):
+func _on_scene_12_connector_body_exited(_body):
 	if firstto2:
 		limit_right = 2880
 		limit_left = 960
@@ -11,3 +12,14 @@ func _on_scene_12_connector_body_exited(body):
 		limit_right = 960
 		limit_left = -960
 		firstto2=true
+
+
+func _on_scene_23_connector_body_exited(_body):
+	if secondto3:
+		limit_top = -1620
+		limit_bottom = -540
+		secondto3=false
+	else:
+		limit_top = -540
+		limit_bottom = 540
+		secondto3=true
